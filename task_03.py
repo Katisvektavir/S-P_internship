e1bcc9a085c9523497e7bc37b811080fcb6d0af1
Katisvektavir <86384829+Katisvektavir@users.noreply.github.com>
2026-02-04 16:11:23 +0300

    Update task_03.py
    Улучшил имена переменных

--- task_03.py
+++ task_03.py
@@ -1,9 +1,9 @@
 def max_odd(array):
-    now_max = 0
+    current_max = 0
     for i in array:
         if (isinstance(i, (int, float))
                 and i % 1 == 0
                 and i % 2 != 0
-                and now_max < i):
-            now_max = i
-    return now_max if now_max != 0 else None
+                and current_max < i):
+            current_max = i
+    return current_max if current_max != 0 else None
