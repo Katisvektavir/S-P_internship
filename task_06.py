044e0537c14e2e45275a51f9027aa6a8abc1d547
Katisvektavir <86384829+Katisvektavir@users.noreply.github.com>
2026-02-07 01:39:39 +0300

    Update task_06.py
    эффективная стратегия игры и лучшие имена переменных

--- task_06.py
+++ task_06.py
@@ -5,28 +5,12 @@ class WrongNumberOfPlayersError(Exception):
 class NoSuchStrategyError(Exception):
     pass
 
-
-def rps_game_winner(list):
-    if len(list) == 2:
-        p1 = list[0]
-        p2 = list[1]
-        p1_choice = list[0][1]
-        p2_choice = list[1][1]
-        if (p1_choice in ("R", "P", "S") and p2_choice in ("R", "P", "S")):
-            if p1_choice == p2_choice:
-                return " ".join(p1)
-            if p1_choice == "R" and p2_choice == "S":
-                return " ".join(p1)
-            if p1_choice == "R" and p2_choice == "P":
-                return " ".join(p2)
-            if p1_choice == "P" and p2_choice == "R":
-                return " ".join(p1)
-            if p1_choice == "P" and p2_choice == "S":
-                return " ".join(p2)
-            if p1_choice == "S" and p2_choice == "P":
-                return " ".join(p1)
-            if p1_choice == "S" and p2_choice == "R":
-                return " ".join(p2)
+def rps_game_winner(game):
+    rules = "RPSR"
+    if len(game) == 2:
+        move = "".join(list(zip(*game))[1])
+        if all(c in rules for c in move):
+            return " ".join(game[1] if move in rules else game [0])
         else:
             raise NoSuchStrategyError
     else:
