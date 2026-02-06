28e4fb13a02483478cdb5cd8709433d669660baa
Katisvektavir <86384829+Katisvektavir@users.noreply.github.com>
2026-02-07 02:06:33 +0300

    Update task_08.py
    теперь программа проходит тесты

--- task_08.py
+++ task_08.py
@@ -1,4 +1,8 @@
 from functools import reduce
 
-def multiply_numbers(inputs):
-    return reduce(lambda x, y: x * y, [int(w) for w in str(inputs) if w.isdigit()])
+
+def multiply_numbers(inputs=None):
+    if inputs and any(char.isdigit() for char in str(inputs)):
+        return reduce(lambda x, y: x * y, [int(w) for w in str(inputs) if w.isdigit()])
+    else:
+        return None
