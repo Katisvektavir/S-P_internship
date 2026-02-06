5a390ed33ed675aa627292aacf09e3beb2bc9a2a
Katisvektavir <86384829+Katisvektavir@users.noreply.github.com>
2026-02-07 01:14:03 +0300

    Update task_01.py
    теперь программа не реагирует на заглавные буквы, пробелы и знаки препинания

--- task_01.py
+++ task_01.py
@@ -1,2 +1,4 @@
-def is_palindrome(string):
-    return str(string) == str(string)[::-1]
+def is_palindrome(text):
+    cleaned_text = str(text).lower()
+    letters_only = ''.join(ch for ch in cleaned_text if ch.isalnum())
+    return letters_only == letters_only[::-1]
