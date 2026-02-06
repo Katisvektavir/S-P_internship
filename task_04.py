657a73976f45e7d5d60784c8d0805b006e3031ef
Katisvektavir <86384829+Katisvektavir@users.noreply.github.com>
2026-02-07 01:03:03 +0300

    Update task_04.py
    Верный ответ в случае передачи пустого списка

--- task_04.py
+++ task_04.py
@@ -1,6 +1,6 @@
 def sort_list(int_list):
     if not int_list:
-        return
+        return []
     M = max(int_list)
     m = min(int_list)
     result = [M if i == m else m if i == M else i for i in int_list]
